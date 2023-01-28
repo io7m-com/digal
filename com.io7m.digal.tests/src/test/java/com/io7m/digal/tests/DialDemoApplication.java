2e1a909324b5807ade063c1e815b7c97853a6de5
Mark Raynsford <code@io7m.com>
2023-01-28 16:29:52 +0000

    Add a snapping converter for real values.

--- com.io7m.digal.tests/src/test/java/com/io7m/digal/tests/DialDemoApplication.java
+++ com.io7m.digal.tests/src/test/java/com/io7m/digal/tests/DialDemoApplication.java
@@ -16,17 +16,24 @@
 
 package com.io7m.digal.tests;
 
+import com.io7m.digal.core.DialBoundedDoubleConverter;
+import com.io7m.digal.core.DialBoundedDoubleSnappingConverter;
+import com.io7m.digal.core.DialBoundedLongConverter;
 import com.io7m.digal.core.DialControl;
-import com.io7m.digal.core.DialValueConverterRealType;
-import com.io7m.digal.core.DialValueConverterType;
 import javafx.application.Application;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+
+import java.util.function.Consumer;
 
 /**
  * A demo application for the dial.
@@ -43,50 +50,47 @@ public final class DialDemoApplication extends Application
   public void start(
     final Stage stage)
   {
-    final var pane = new StackPane();
+    final var pane = new FlowPane(Orientation.HORIZONTAL);
     pane.setPrefSize(640, 480);
     pane.setPadding(new Insets(8));
 
-    final var dial0 = new DialControl();
+    final var children = pane.getChildren();
+    children.add(dial(d -> {
+      d.setTickCount(24);
+      d.setValueConverter(new DialBoundedLongConverter(0L, 24L, 1L));
+      d.setConvertedValue(0.0);
+    }));
+
+    children.add(dial(d -> {
+      d.setTickCount(20);
+      d.setValueConverter(new DialBoundedDoubleConverter(-1.0, 1.0, 0.1));
+      d.setConvertedValue(0.0);
+    }));
+
+    children.add(dial(d -> {
+      d.setTickCount(16 * 2);
+      d.setValueConverter(new DialBoundedDoubleSnappingConverter(-8.0, 8.0, 0.5));
+      d.setConvertedValue(0.0);
+    }));
+
+    final var scene = new Scene(pane);
+    stage.setTitle("Dial Control");
+    stage.setScene(scene);
+    stage.show();
+  }
+
+  private static Pane dial(
+    final Consumer<DialControl> configurator)
+  {
     final var dialSize = 128.0;
-    dial0.setPrefSize(dialSize, dialSize);
-    dial0.setMinSize(dialSize, dialSize);
-    dial0.setMaxSize(dialSize, dialSize);
 
-    dial0.dialRadialGaugeSize().setValue(12.0);
+    final var dial = new DialControl();
+    dial.setPrefSize(dialSize, dialSize);
+    dial.setMinSize(dialSize, dialSize);
+    dial.setMaxSize(dialSize, dialSize);
+    dial.dialRadialGaugeSize().setValue(12.0);
 
-    dial0.setTickCount(24);
-    dial0.setValueConverter(
-      new DialValueConverterRealType()
-      {
-        @Override
-        public double convertToDial(
-          final double x)
-        {
-          return x / 12.0;
-        }
-
-        @Override
-        public double convertFromDial(
-          final double x)
-        {
-          return (double) Math.round(x * 12.0);
-        }
-
-        @Override
-        public double convertedNext(
-          final double x)
-        {
-          return x + 1.0;
-        }
-
-        @Override
-        public double convertedPrevious(
-          final double x)
-        {
-          return x - 1.0;
-        }
-      });
+    configurator.accept(dial);
 
     final var field = new TextField();
     field.setMaxSize(64, 16);
@@ -94,14 +98,12 @@ public final class DialDemoApplication extends Application
     field.setEditable(false);
     field.setAlignment(Pos.CENTER);
     field.setFont(Font.font("Monospaced", 10));
-    field.textProperty().bind(dial0.convertedValue().map(Object::toString));
+    field.textProperty().bind(dial.convertedValue().map(Object::toString));
 
-    pane.getChildren()
-      .addAll(dial0, field);
-
-    final var scene = new Scene(pane);
-    stage.setTitle("Dial Control");
-    stage.setScene(scene);
-    stage.show();
+    final var region = new Region();
+    region.setPrefSize(128.0 + 16.0, 128.0 + 56);
+    region.setMinSize(128.0 + 16.0, 128.0 + 56);
+    region.setMaxSize(128.0 + 16.0, 128.0 + 56);
+    return new StackPane(region, dial, field);
   }
 }
